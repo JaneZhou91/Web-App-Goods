0fd4ddb6b13e00b22e11292684ba9fe55e871430
JaneZhou <1294642551@qq.com>
2017-09-15 11:37:00 +0800

    0915commit

---
+++ app/containers/Hello/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Hello extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+    }
+    render() {
+        return (
+            <div>
+                Hello World !
+            </div>
+        );
+    }
+
+}
+
+export default Hello;
--- webpack.config.js
+++ webpack.config.js
@@ -10,22 +10,28 @@ module.exports = {
         path: __dirname + "/build",
         filename: "bundle.js"
     },
+    resolve:{
+        extensions:['.js','.jsx']
+    },
     module: {
-        loaders: [
+        rules: [
             { 
             	test: /\.(js|jsx)$/, 
             	exclude: /node_modules/, 
-            	loader: 'babel' 
+            	loader: 'babel-loader',
+                options: {
+                    presets: ["es2015"]
+                },
             },
             { 
             	test: /\.less$/, 
             	exclude: /node_modules/, 
-            	loader: 'style!css!postcss!less' 
+            	loader: 'style-loader!css-loader!less-loader' 
             },
             { 
             	test: /\.css$/, 
             	exclude: /node_modules/, 
-            	loader: 'style!css!postcss' 
+            	loader: 'style-loader!css-loader' 
             },
             { 
             	test:/\.(png|gif|jpg|jpeg|bmp)$/i, 
@@ -37,4 +43,52 @@ module.exports = {
             } 
         ]
     },
+
+    resolve: {
+        // 解析模块请求的选项
+        // （不适用于对 loader 解析）
+
+        modules: [// 用于查找模块的目录
+          "node_modules",
+           path.resolve(__dirname, "app")
+        ],
+        
+
+        extensions: [".js", ".json", ".jsx", ".css"],
+    },
+
+
+    plugins: [
+        // html 模板插件
+        new HtmlWebpackPlugin({
+            template: __dirname + '/app/index.tmpl.html'
+        }),
+
+        // 热加载插件
+        new webpack.HotModuleReplacementPlugin(),
+
+        // 打开浏览器
+        new OpenBrowserPlugin({
+          url: 'http://localhost:8080'
+        }),
+
+        // 可在业务 js 代码中使用 __DEV__ 判断是否是dev模式（dev模式下可以提示错误、测试报告等, production模式不提示）
+        new webpack.DefinePlugin({
+          __DEV__: JSON.stringify(JSON.parse((process.env.NODE_ENV == 'dev') || 'false'))
+        }),
+
+        new webpack.LoaderOptionsPlugin({
+            options: {
+
+                devServer: {
+                    contentBase: "./public", //本地服务器所加载的页面所在的目录
+                    colors: true, //终端中输出结果为彩色
+                    historyApiFallback: true, //不跳转
+                    hot: true,  // 使用热加载插件 HotModuleReplacementPlugin
+                    inline: true //实时刷新
+                }
+            }
+        })
+    ],
+
 }
